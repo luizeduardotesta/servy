b5ead9016ba248948eed7513a439f62352bd4f8f
luizeduardotesta <luizsonecazzz@hotmail.com>
2022-02-15 17:05:19 -0300

    add route and format response

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,27 +2,32 @@ defmodule Servy.Handler do
   def handler(request) do
     request
     |> parse
-    |>route
-    |>format_response
+    |> route
+    |> format_response
   end
 
   def parse(request) do
-    [method,  path, _] =
+    [method, path, _] =
       request
       |> String.split("\n")
-      |> List.first
+      |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: "" }
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bear, Lions and Tigers" }
+    %{conv | resp_body: "Bear, Lions and Tigers"}
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
   end
 end
 
